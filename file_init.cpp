a10cd18e228d8c5bf1c0c8585b0e653700e11d2b
channy <349117102@qq.com>
2020-05-16 08:36:11 +0800

    new dev branch

--- file_init.cpp
+++ file_init.cpp
@@ -3,7 +3,7 @@
 int main() {
 	int res = 0;
 	std::cout << __FILE__ << " " << __FUNCTION__ << " " << __LINE__ << std::endl;
-	std::cout << "add master output" << std::endl;
+	std::cout << "add dev output" << std::endl;
 	return res * 2;
 }
 
